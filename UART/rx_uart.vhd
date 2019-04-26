57092c0f6b646f6a4a0b60804f1666406a8a390b
JorisP <jor.pell@hotmail.fr>
2019-04-26 11:55:51 +0200

    MAJ RX uart

--- UART/rx_uart.vhd
+++ UART/rx_uart.vhd
@@ -53,11 +53,17 @@ architecture arch of rx_uart is
   constant number_of_bits    : integer := number_of_bit_computation(stop_bit_number, parity, data_size);  -- number of bit in the transaction
 
   -- SIGNALS
-  signal rx_fsm      : t_rx_fsm;        -- RX uart FSM
-  signal rx_old      : std_logic;       -- Latch RX input
-  signal start_rx_fe : std_logic;       -- Start bit Falling edge detected
-  signal start_rx_re : std_logic;       -- Start_bit rising edge detected
-begin
+  signal rx_fsm       : t_rx_fsm;       -- RX uart FSM
+  signal rx_old       : std_logic;      -- Latch RX input
+  signal start_rx_fe  : std_logic;      -- Start bit Falling edge detected
+  signal start_rx_re  : std_logic;      -- Start_bit rising edge detected
+  signal start_cnt    : std_logic;  -- Start counter in order to generate Tick
+  signal tick_data    : std_logic;  -- Tick data in order to read the RX input
+  signal rx_data_s    : std_logic_vector(data_size - 1 downto 0);  -- Save the RX data input
+  -- COUNTERS
+  signal cnt_half_bit : integer range 0 to half_bit_duration;  -- Counter halft bit in order to sample the data
+  signal cnt_bit      : integer range 0 to bit_duration;  -- Counter bit duration
+  signal cnt_data     : integer range 0 to data_size;  -- Counter of the data
 
   -- purpose : This process manages the FSM on the RX UART
   p_rx_fsm_mng : process (clock, reset_n) is
@@ -79,6 +85,17 @@ begin
             end if;
           end if;
         when READ_START =>
+          if(tick_data = '1') then
+            rx_fsm <= READ_DATA;
+          end if;
+        when READ_DATA =>
+          if(tick_data = '1' and cnt_data = data_size - 1) then
+            if(parity /= none) then
+              rx_fsm <= READ_PARITY;
+            else
+              rx_fsm <= READ_STOP_BIT;
+            end if;
+          end if;
         when others => null;
       end case;
     end if;
@@ -107,10 +124,52 @@ begin
   -- purpose: This process generates ticks in order to read the input data 
   p_tick_read_data : process (clock, reset_n) is
   begin  -- process p_tick_read_data
+    if reset_n = '0' then                   -- asynchronous reset (active low)
+      cnt_half_bit <= 0;
+      start_cnt    <= '0';
+      tick_data    <= '0';
+    elsif clock'event and clock = '1' then  -- rising clock edge
+      if(rx_fsm = WAIT_START) then
+        if(start_rx_re = '1' or start_rx_fe = '1') then
+          start_cnt <= '1';
+        end if;
+      elsif(rx_fsm = READ_START) then
+        if(cnt_half_bit < half_bit_duration) then
+          cnt_half_bit <= cnt_half_bit + 1;
+          tick_data    <= '0';
+        else
+          cnt_half_bit <= 0;
+          tick_data    <= '1';
+        end if;
+      elsif(rx_fsm = READ_DATA) then
+        if(cnt_bit < bit_duration) then
+          cnt_bit   <= cnt_bit + 1;
+          tick_data <= '0';
+        else
+          cnt_bit   <= 0;
+          tick_data <= '1';
+        end if;
+      end if;
+    end if;
+  end process p_tick_read_data;
+
+  -- purpose: This process count the data to received 
+  p_data_cnt : process (clock, reset_n) is
+  begin  -- process p_data_cnt
     if reset_n = '0' then               -- asynchronous reset (active low)
 
     elsif clock'event and clock = '1' then  -- rising clock edge
 
     end if;
-  end process p_tick_read_data;
+  end process p_data_cnt;
+
+
+--   if reset_n = '0' then                 -- asynchronous reset (active low)
+
+--   elsif clock'event and clock = '1' then  -- rising clock edge
+
+--   end if;
+-- end process p_read_rx;
+
+
 end architecture arch;
