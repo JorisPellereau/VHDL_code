5b784ba3d8d16d16813f33a9b5a4ba38e5478937
Joris P <jor.pell@hotmail.fr>
2019-04-28 14:17:18 +0200

    Add RX UART component

--- UART/pkg_rs232.vhd
+++ UART/pkg_rs232.vhd
@@ -6,7 +6,7 @@
 -- Author     :   
 -- Company    : 
 -- Created    : 2019-04-24
--- Last update: 2019-04-26
+-- Last update: 2019-04-28
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -79,6 +79,24 @@ package pkg_rs232 is
       tx       : out std_logic;         -- Serial output transmission
       tx_done  : out std_logic);        -- Transaction done
   end component;
+
+  component rx_uart is
+    generic (
+      stop_bit_number : integer range 1 to 2 := 1;  -- Number of stop bit
+      parity          : t_parity             := even;  -- Type of the parity
+      baudrate        : t_baudrate           := b9600;      -- Baudrate
+      data_size       : integer range 5 to 9 := 8;  -- Size of the data to transmit
+      polarity        : std_logic            := '1';  -- Polarity in idle state
+      first_bit       : t_first_bit          := lsb_first;  -- LSB or MSB first
+      clock_frequency : integer              := 20000000);  -- Clock frequency [Hz]
+    port (
+      reset_n     : in  std_logic;      -- Asynchronous reset
+      coc         : in  std_logic :     -- Clock
+      rx          : in  std_logic;      -- Serial input
+      rx_data     : out std_logic_vector(data_size - 1 downto 0);  -- Received data
+      rx_done     : out std_logic;      -- Flag for a received data
+      parity_rcvd : out std_logic);     -- Parity received
+  end component rx_uart;
 
 end package pkg_rs232;
 
