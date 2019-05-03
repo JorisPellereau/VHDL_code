2ca943194482c4b727ef1c83a11cd76ab503b877
JorisP <jor.pell@hotmail.fr>
2019-05-03 17:16:59 +0200

    Add component

--- SPI/pkg_spi.vhd
+++ SPI/pkg_spi.vhd
@@ -19,6 +19,9 @@
 -- 2019-05-03  1.0      pellereau       Created
 -------------------------------------------------------------------------------
 
+library ieee;
+use ieee.std_logic_1164.all;
+
 package pkg_spi is
 
   -- SPI MODES
@@ -38,4 +41,25 @@ package pkg_spi is
   constant T_sclk           : integer := clock_frequency / spi_frequency;  -- Period of the SPI clock
   constant T_2_sclk         : integer := T_sclk / 2;  -- Half sclk period
 
+
+  -- COMPONENTS
+  component master_spi is
+    generic (
+      cpol         : std_logic                           := '0';  -- Clock Polarity
+      cpha         : std_logic                           := '0';  -- Clock phase
+      data_size    : integer                             := 8;  -- Size of the data
+      slave_number : integer range 1 to max_slave_number := max_slave_number);  -- Number of slave
+
+    port (
+      reset_n   : in  std_logic;        -- Asynchronous reset
+      clock     : in  std_logic;        -- System clock
+      ssi       : in  std_logic_vector(slave_number - 1 downto 0);  -- Slave Select input
+      start_spi : in  std_logic;        -- Start SPI transaction
+      wdata     : in  std_logic_vector(data_size - 1 downto 0);  -- Data to transmit
+      miso      : in  std_logic;        -- Master In Slave Out
+      mosi      : out std_logic;        -- Master Out Slave In
+      sclk      : out std_logic;        -- Serial Clock
+      ssn       : out std_logic_vector(slave_number - 1 downto 0);  -- Slave Select
+      rdata     : out std_logic_vector(data_size - 1 downto 0));
+  end component;
 end package pkg_spi;
