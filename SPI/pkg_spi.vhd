4d6fba2685c9c040df574a56c5b5ca042ef3fe35
JorisP <jor.pell@hotmail.fr>
2019-05-03 11:33:49 +0200

    Add constants to package

--- SPI/pkg_spi.vhd
+++ SPI/pkg_spi.vhd
@@ -21,6 +21,21 @@
 
 package pkg_spi is
 
+  -- SPI MODES
+  -- MODE 0 : CPOL : 0 - CPHA : 0
+  -- MODE 1 : CPOL : 0 - CPHA : 1
+  -- MODE 2 : CPOL : 1 - CPHA : 0
+  -- MODE 3 : CPOL : 1 - CPHA : 1
 
+  -- CONSTANTS
+
+  -- System constants
+  constant clock_frequency : integer := 50000000;  -- Input clock frequency
+
+  -- SPI module constants
+  constant max_slave_number : integer := 2;  -- Number of slave on the SPI bus
+  constant spi_frequency    : integer := 5000000;     -- SLCK SPI frequency
+  constant T_sclk           : integer := clock_frequency / spi_frequency;  -- Period of the SPI clock
+  constant T_2_sclk         : integer := T_sclk / 2;  -- Half sclk period
 
 end package pkg_spi;
