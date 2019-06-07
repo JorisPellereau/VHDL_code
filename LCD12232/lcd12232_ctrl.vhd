b198991a479368bc0c87a241d3f30e0837578018
Joris P <jor.pell@hotmail.fr>
2019-06-07 16:14:32 +0200

    add constant  + ctrl

--- LCD12232/lcd12232_ctrl.vhd
+++ LCD12232/lcd12232_ctrl.vhd
@@ -21,6 +21,7 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
 
 library lib_lcd12232;
 use lib_lcd12232.pkg_lcd12232.all;
@@ -35,14 +36,97 @@ entity lcd12232_ctrl is
     en2_o     : out   std_logic;        -- Enable for IC2
     rw_o      : out   std_logic;        -- Read/Write selection
     rst_o     : out   std_logic;        -- LCD reset
-    db_o      : inout std_logic_vector(7 downto 0));  -- Data to LCD
+    data_io   : inout std_logic_vector(7 downto 0));  -- Data to LCD
 
 end entity lcd12232_ctrl;
 
 architecture arch_lcd12232_ctrl of lcd12232_ctrl is
 
+
+  -- SIGNALS
+  signal starts_rw_s : std_logic_vector;  -- Start a RW transaction on the bus
+  signal run_rw_s    : std_logic;         -- Run RW on the bus
+  signal rw_s        : std_logic;         -- R/W command  
+  signal a0_s        : std_logic;         -- A0 signal
+
+  -- Data IO signals
+  signal en_data_io_s : std_logic;      -- Enable the R or Write on db_o
+  signal data_i_s     : std_logic_vector(7 downto 0);  -- Data from the bus
+  signal data_o_s     : std_logic_vector(7 downto 0);  -- Data to the bus
+
+  signal en1_o_s : std_logic;           -- Enable 1 signal
+  signal en2_o_s : std_logic;           -- Enable 2 signal
+  signal rw_o_s  : std_logic;           -- RW output signal
+
+  signal wdata_s : std_logic_vector(7 downto 0);  -- Data to write on the bus
+  signal rdata_s : std_logic_vector(7 downto 0);  -- Data read from the bus
+  signal rw_i_s  : std_logic;                     -- RW command
+  signal a0_i_s  : std_logic;                     -- a0 command
+
+  -- COUNTERS
+  signal cnt_1us_s      : unsigned(5 downto 0);  -- Counter until 0x32
+  signal cnt_1us_done_s : std_logic;             -- Max cnt 1 us reach
+
 begin  -- architecture arch_lcd12232_ctrl
 
 
+
+  -- purpose: This process manages the Read or Write transaction on the bus
+  p_RW_manage : process (clock_i, reset_n_i) is
+  begin  -- process p_RW_manage
+    if reset_n_i = '0' then                 -- asynchronous reset (active low)
+      run_rw_s     <= '0';
+      en_data_io_s <= '0';
+      en1_o_s      <= '0';
+      en2_o_s      <= '0';
+    elsif clock'event and clock = '1' then  -- rising clock edge
+      if(starts_rw_s = '1') then
+        run_rw_s <= '1';
+      end if;
+
+      if(run_rw_s = '1') then
+        rw_o_s <= rw_i_s;
+        a0_s   <= a0_i_s;
+        if(cnt_1us_done_s = '1') then
+          en1_o_s <= not en1_o_s;
+          en2_o_s <= not en2_o_s;
+        end if;
+
+      end if;
+    end if;
+  end process p_RW_manage;
+
+
+  en1_o     <= en1_o_s;
+  en2_o     <= en2_o_s;
+  rw_o      <= rw_o_s;
+  reg_sel_o <= a0_s;
+
+  data_io  <= data_o_s when en_data_io_s = '1' else 'Z';  -- Write on the bus
+  data_i_s <= data_io;                                    -- Read from the bus
+
+
+  -- purposeThis process counts until 50 in order to control enables outputs
+  p_cnt_1us : process(clock_i, reset_n_i)
+  begin  -- process p_cnt_1us
+    if reset_n_i = '0' then                 -- asynchronous reset (active low)
+      cnt_1us_s      <= (others => '0');
+      cnt_1us_done_s <= '0';
+    elsif clock'event and clock = '1' then  -- rising clock edge
+      if(run_rw_s = '1') then
+        if(cnt_1us_s < C_MAX_CNT_1U) then
+          cnt_1us_done_s <= '0';
+          cnt_1us_s      <= cnt_1us_s + 1;
+        else
+          cnt_1us_done_s <= '1';
+          cnt_1us_s      <= 0;
+        end if;
+      else
+        cnt_1us_s      <= (others => '0');
+        cnt_1us_done_s <= '0';
+      end if;
+
+    end if;
+  end process p_cnt_1us;
 
 end architecture arch_lcd12232_ctrl;
--- LCD12232/pkg_lcd12232.vhd
+++ LCD12232/pkg_lcd12232.vhd
@@ -24,6 +24,27 @@ use ieee.std_logic_1164.all;
 
 package pkg_lcd12232 is
 
+  -- Inputs clock : 50MHz : Tclk = 20 ns
+
+  -- CONSTANTS
+  constant C_DISPLAY_ON       : std_logic_vector(7 downto 0) := x"AF";  -- Display on
+  constant C_DISPLAY_OFF      : std_logic_vector(7 downto 0) := x"AE";  -- Display off
+  constant C_SET_PAGE_0       : std_logic_vector(7 downto 0) := x"B8";  -- Page address set 0
+  constant C_SET_PAGE_1       : std_logic_vector(7 downto 0) := x"B9";  -- Page address set 1
+  constant C_SET_PAGE_2       : std_logic_vector(7 downto 0) := x"BA";  -- Page address set 2
+  constant C_SET_PAGE_3       : std_logic_vector(7 downto 0) := x"BB";  -- Page address set 3
+  constant C_RIGHTWARD        : std_logic_vector(7 downto 0) := x"A0";  -- Rightward output
+  constant C_LEFTWARD         : std_logic_vector(7 downto 0) := x"A1";  -- Leftward output
+  constant C_STATIC_DRIVE_ON  : std_logic_vector(7 downto 0) := x"A4";  -- Normal display operation
+  constant C_STATIC_DRIVE_OFF : std_logic_vector(7 downto 0) := x"A5";  --Power save
+  constant C_DUTY_1_16        : std_logic_vector(7 downto 0) := x"A8";  -- 1/16 duty factor LCD
+  constant C_DUTY_1_32        : std_logic_vector(7 downto 0) := x"A9";  -- 1/32 duty factor LCD
+  constant C_RD_MODIFY_WR     : std_logic_vector(7 downto 0) := x"E0";  -- +1 column addr
+  constant C_END              : std_logic_vector(7 downto 0) := x"EE";  -- Cancels RD_modif mode
+  constant C_RST_DISPLAY      : std_logic_vector(7 downto 0) := x"E2";  -- Reset display
+
+
+  constant C_MAX_CNT_1US : unsigned(5 downto 0) := x"32";  -- Max coutner for 1 us
 
 
 end package pkg_lcd12232;
