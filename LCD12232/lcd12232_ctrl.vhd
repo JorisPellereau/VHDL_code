d35dc34190435866e8d620e4c074dababdd9653c
JorisP <jor.pell@hotmail.fr>
2019-06-10 17:21:21 +0200

    Add main FSM- ongoing

--- LCD12232/lcd12232_ctrl.vhd
+++ LCD12232/lcd12232_ctrl.vhd
@@ -44,12 +44,16 @@ architecture arch_lcd12232_ctrl of lcd12232_ctrl is
 
 
   -- SIGNALS
-  signal start_rw_s : std_logic;        -- Start a RW transaction on the bus
-  signal rw_s       : std_logic;        -- R/W command  
+  signal start_rw_s : std_logic;        -- Start a RW transaction on the bus 
   signal a0_s       : std_logic;        -- A0 signal
 
 
-  signal fsm_rw_s : t_fsm_rw;           -- FSM states
+  signal fsm_rw_s  : t_fsm_rw;          -- FSM states RW
+  signal rw_done_s : std_logic;         -- Transaction done
+
+  signal fsm_ctrl_s : t_fsm_ctrl;       -- FSM of the controller
+
+
 
   -- Data IO signals
   signal en_data_io_s : std_logic;      -- Enable the R or Write on db_o
@@ -59,6 +63,7 @@ architecture arch_lcd12232_ctrl of lcd12232_ctrl is
   signal en1_o_s : std_logic;           -- Enable 1 signal
   signal en2_o_s : std_logic;           -- Enable 2 signal
   signal rw_o_s  : std_logic;           -- RW output signal
+  signal rst_o_s : std_logic;           -- LCD rst signal
 
   signal wdata_s : std_logic_vector(7 downto 0);  -- Data to write on the bus
   signal rdata_s : std_logic_vector(7 downto 0);  -- Data read from the bus
@@ -73,10 +78,92 @@ architecture arch_lcd12232_ctrl of lcd12232_ctrl is
   signal cnt_tacc_rd_s      : unsigned(2 downto 0);  -- Counter until 5
   signal cnt_tacc_rd_done_s : std_logic;             -- Max cnt reach
 
+  signal cnt_rst_s      : unsigned(7 downto 0);  -- LCd counter reset
+  signal cnt_rst_done_s : std_logic;             -- Max cnt reset reach
 
 begin  -- architecture arch_lcd12232_ctrl
 
 
+  -- ==== MAIN FSM MANAGEMENT ==
+
+  -- purpose: This process manages the LCd controller 
+  p_fsm_controller : process (clock_i, reset_n_i)
+  begin  -- process p_fsm_controller
+    if reset_n_i = '0' then             -- asynchronous reset (active low)
+      fsm_ctrl_s <= IDLE;
+    elsif clock_i'event and clock_i = '1' then  -- rising clock edge
+      case fsm_ctrl_s is
+
+        when IDLE =>
+          fsm_ctrl_s <= RST_LCD;
+
+        when RST_LCD =>
+          if(cnt_rst_done_s = '1') then
+            fsm_ctrl_s <= INIT_LCD;
+          end if;
+
+        when INIT_LCD =>
+
+        when others => null;
+      end case;
+    end if;
+  end process p_fsm_controller;
+
+
+  -- purpose: This process manages the reset of the LCD 
+  p_cnt_rst_mng : process (clock_i, reset_n_i)
+  begin  -- process p_cnt_rst_mng
+    if reset_n_i = '0' then             -- asynchronous reset (active low)
+      cnt_rst_s      <= (others => '0');
+      cnt_rst_done_s <= '0';
+    elsif clock_i'event and clock_i = '1' then  -- rising clock edge
+      if(fsm_ctrl_s = RST_LCD) then
+        if(cnt_rst_s < C_MAX_CNT_1US - 1) then
+          cnt_rst_s      <= cnt_rst_s + 1;
+          cnt_rst_done_s <= '0';
+        else
+          cnt_rst_s      <= (others => '0');
+          cnt_rst_done_s <= '1';
+        end if;
+      else
+        cnt_rst_s      <= (others => '0');
+        cnt_rst_done_s <= '0'
+      end if;
+    end if;
+  end process p_cnt_rst_mng;
+
+
+  -- ==== END MAIN FSM MANAGEMENT ==
+
+
+
+
+  -- DEBUG purpose: This process manages the send of the command on the bus
+  p_start_rw_mng : process (clock_i, reset_n_i)
+    variable v_en_start : std_logic := '0';  -- init for test
+  begin  -- process p_start_rw_mng
+    if reset_n_i = '0' then                  -- asynchronous reset (active low)
+      start_rw_s <= '0';
+      rw_i_s     <= '0';
+      a0_i_s     <= '0';
+      wdata_s    <= (others => '0');
+      rdata_s    <= (others => '0');
+      v_en_start := '0';
+
+    elsif clock_i'event and clock_i = '1' then  -- rising clock edge
+      if(rw_done_s = '1' and v_en_start = '0') then
+        start_rw_s <= '1';
+        rw_i_s     <= '1';
+        a0_i_s     <= '1';
+        v_en_start := '1';
+      else
+        start_rw_s <= '0';
+      end if;
+    end if;
+  end process p_start_rw_mng;
+
+
+  -- ==== RW BUS MANAGEMENT ==
 
   -- purpose: This process manages the bus RW
   p_fsm_rw_mng : process (clock_i, reset_n_i)
@@ -90,6 +177,7 @@ begin  -- architecture arch_lcd12232_ctrl
       en_data_io_s    <= '0';           -- Set 'Z' on the bus
       data_o_s        <= (others => '0');
       start_cnt_1us_s <= '0';
+      rw_done_s       <= '1';
     elsif clock_i'event and clock_i = '1' then  -- rising clock edge
       case fsm_rw_s is
 
@@ -99,6 +187,7 @@ begin  -- architecture arch_lcd12232_ctrl
             en1_o_s         <= '0';
             en2_o_s         <= '0';
             start_cnt_1us_s <= '1';
+            rw_done_s       <= '1';
           end if;
 
         when SET_RW_REG =>
@@ -129,6 +218,7 @@ begin  -- architecture arch_lcd12232_ctrl
           if(cnt_tacc_rd_done_s = '1') then
             rdata_s <= data_i_s;
           end if;
+
           if(cnt_1us_done_s = '1') then
             fsm_rw_s        <= RST_ENi;
             start_cnt_1us_s <= '0';
@@ -142,6 +232,7 @@ begin  -- architecture arch_lcd12232_ctrl
         when RST_DATA =>
           en_data_io_s <= '0';          -- Set 'Z' on the bus
           data_o_s     <= (others => '0');
+          rw_done_s    <= '0';
           fsm_rw_s     <= IDLE;
 
         when others => null;
@@ -170,8 +261,9 @@ begin  -- architecture arch_lcd12232_ctrl
       cnt_tacc_rd_s      <= (others => '0');
       cnt_tacc_rd_done_s <= '0';
     elsif clock_i'event and clock_i = '1' then  -- rising clock edge
-      if(start_cnt_1us_s = '1') then    -- A voir
-        if(cnt_1us_s < C_MAX_CNT_1US) then
+
+      if(start_cnt_1us_s = '1') then
+        if(cnt_1us_s < C_MAX_CNT_1US - 1) then
           cnt_1us_done_s <= '0';
           cnt_1us_s      <= cnt_1us_s + 1;
         else
@@ -184,11 +276,13 @@ begin  -- architecture arch_lcd12232_ctrl
       end if;
 
       if(fsm_rw_s = RD_DATA) then
-        if((cnt_tacc_rd_s < C_MAX_TACC_RD) and cnt_tacc_rd_done_s = '0') then
+        if((cnt_tacc_rd_s < C_MAX_TACC_RD - 1) and cnt_tacc_rd_done_s = '0') then
           cnt_tacc_rd_s <= cnt_tacc_rd_s + 1;  -- Inc cnt
-        else
+        elsif(cnt_tacc_rd_s = C_MAX_TACC_RD - 1) then
           cnt_tacc_rd_s      <= (others => '0');
           cnt_tacc_rd_done_s <= '1';
+        elsif(cnt_tacc_rd_done_s = '1') then
+          cnt_tacc_rd_done_s <= '0';
         end if;
       else
         cnt_tacc_rd_s      <= (others => '0');
@@ -197,5 +291,8 @@ begin  -- architecture arch_lcd12232_ctrl
 
     end if;
   end process p_cnt_1us;
+
+  -- == END RW BUS MANAGEMENT ==
+
 
 end architecture arch_lcd12232_ctrl;
