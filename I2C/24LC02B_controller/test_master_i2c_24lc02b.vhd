b817010eae03be41d370ecd987601040e3f62c26
Joris P <jor.pell@hotmail.fr>
2019-06-26 17:08:47 +0200

    up test

--- I2C/24LC02B_controller/test_master_i2c_24lc02b.vhd
+++ I2C/24LC02B_controller/test_master_i2c_24lc02b.vhd
@@ -6,7 +6,7 @@
 -- Author     :   <JorisPC@JORISP>
 -- Company    : 
 -- Created    : 2019-06-25
--- Last update: 2019-06-25
+-- Last update: 2019-06-26
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -32,17 +32,40 @@ end entity test_master_i2c_24c02b;
 architecture arch_test_master_i2c_24lc02b of test_master_i2c_24c02b is
 
   -- COMPONENT
-  component master_i2c_24lc02b is
+  -- component master_i2c_24lc02b is
 
+  --   generic (
+  --     scl_frequency   : t_i2c_frequency := f100k;  -- Frequency of SCL clock
+  --     clock_frequency : integer         := 20000000);  -- Input clock frequency
+  --   port (
+  --     reset_n    : in    std_logic;     -- Asynchronous reset
+  --     clock      : in    std_logic;     -- Input clock
+  --     start_i2c  : in    std_logic;     -- Start an I2C transaction
+  --     rw         : in    std_logic;     -- Read/Write command
+  --     rd_mode    : in    std_logic_vector(1 downto 0);  -- Read mode selection
+  --     chip_addr  : in    std_logic_vector(6 downto 0);  -- Chip address [0:127]
+  --     nb_data    : in    integer range 1 to max_array;  -- Number of byte to Read or Write
+  --     wdata      : in    t_byte_array;  -- Array of data to transmit
+  --     i2c_done   : out   std_logic;     -- I2C transaction done
+  --     sack_error : out   std_logic;     -- Sack from slave not received
+  --     rdata      : out   t_byte_array;  -- Output data read from an I2C transaction
+  --     scl        : inout std_logic;     -- I2C clock
+  --     sda        : inout std_logic);    -- Data line
+
+  -- end component;
+
+
+  component i2c_eeprom_ctrl
     generic (
-      scl_frequency   : t_i2c_frequency := f100k;  -- Frequency of SCL clock
-      clock_frequency : integer         := 20000000);  -- Input clock frequency
+      scl_frequency   : t_i2c_frequency := f400k;  -- Frequency of SCL clock
+      clock_frequency : integer         := 50000000);  -- Input clock frequency
     port (
       reset_n    : in    std_logic;     -- Asynchronous reset
       clock      : in    std_logic;     -- Input clock
       start_i2c  : in    std_logic;     -- Start an I2C transaction
       rw         : in    std_logic;     -- Read/Write command
       rd_mode    : in    std_logic_vector(1 downto 0);  -- Read mode selection
+      wr_mode    : in    std_logic;     -- Write mode
       chip_addr  : in    std_logic_vector(6 downto 0);  -- Chip address [0:127]
       nb_data    : in    integer range 1 to max_array;  -- Number of byte to Read or Write
       wdata      : in    t_byte_array;  -- Array of data to transmit
@@ -63,6 +86,7 @@ architecture arch_test_master_i2c_24lc02b of test_master_i2c_24c02b is
   signal start_i2c  : std_logic;
   signal rw         : std_logic;
   signal rd_mode    : std_logic_vector(1 downto 0);
+  signal wr_mode    : std_logic;
   signal chip_addr  : std_logic_vector(6 downto 0);
   signal nb_data    : integer range 1 to max_array;
   signal wdata      : t_byte_array;
@@ -89,7 +113,8 @@ begin  -- architecture arch_test_master_i2c_24lc02b
     -- INIT
     start_i2c <= '0';
     rw        <= '0';
-    rd_mode    <= (others => '0');
+    rd_mode   <= (others => '0');
+    wr_mode   <= '0';
     chip_addr <= "1111101";
     nb_data   <= 1;
     wdata     <= (others => (others => '0'));
@@ -113,7 +138,11 @@ begin  -- architecture arch_test_master_i2c_24lc02b
     wait;
   end process p_stimuli;
 
-  eeprom_ctrl_inst : master_i2c_24lc02b
+
+
+
+  -- eeprom_ctrl_inst : master_i2c_24lc02b
+  eeprom_ctrl_inst : i2c_eeprom_ctrl
     generic map (
       scl_frequency   => f400k,
       clock_frequency => 50000000)
@@ -123,6 +152,7 @@ begin  -- architecture arch_test_master_i2c_24lc02b
       start_i2c  => start_i2c,
       rw         => rw,
       rd_mode    => rd_mode,
+      wr_mode    => wr_mode,
       chip_addr  => chip_addr,
       nb_data    => nb_data,
       wdata      => wdata,
