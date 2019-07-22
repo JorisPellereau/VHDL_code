2ad787ef517eb0f80ef4340cfc0f139d4cc7a7a8
Joris P <jor.pell@hotmail.fr>
2019-07-22 12:48:48 +0200

    Add controller component and constants

--- MAX7219/pkg_max7219.vhd
+++ MAX7219/pkg_max7219.vhd
@@ -6,7 +6,7 @@
 -- Author     :   <pellereau@D-R81A4E3>
 -- Company    : 
 -- Created    : 2019-07-19
--- Last update: 2019-07-19
+-- Last update: 2019-07-22
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -27,10 +27,19 @@ package pkg_max7219 is
   -- SYSTEM clock : 50MHz : 20 ns
   -- CLK frequency : 5MHz (10MHz max)
 
-  -- CONSTANTS
+  -- == MAX7219_interface CONSTANTS ==
   constant C_T_CLK    : integer := 10;      -- 50MHz/5MHz = 10
   constant C_T_2_sclk : integer := 10 / 2;  -- Half Period of CLK
+  -- =================================
 
+  -- == MAX7219_controller TYPES & CONSTANTS ==
+  constant C_CFG_NB           : integer                      := 3;  -- Number of configuration
+  constant C_DECODE_MODE_ADDR : std_logic_vector(7 downto 0) := x"09";  -- Decode mode address register
+  constant C_INTENSITY_ADDR   : std_logic_vector(7 downto 0) := x"0A";  -- Inensity addr register
+  constant C_SCAN_LIMIT_ADDR  : std_logic_vector(7 downto 0) := x"0B";  -- Scan limit addr register
+
+  type t_max7219_ctrl_fsm is (IDLE, SET_CFG);  -- States of the MAX7219 Controller
+  -- ==================================
 
   -- COMPONENTS
   component max7219_interface
@@ -43,6 +52,28 @@ package pkg_max7219 is
       data_o        : out std_logic;    -- DATA to send th
       clk_o         : out std_logic;    -- CLK
       frame_done_o  : out std_logic);   -- Frame done
+  end component;
+
+  component max7219_controller is
+    port (
+      clock_i   : in std_logic;         -- System clock
+      reset_n_i : in std_logic;         -- Asynchonous Active Low
+
+      -- From MAX7219 interface
+      frame_done_i : in std_logic;  -- Frame done from the MAX7219 interface
+
+      -- Config inputs
+      start_config_i     : in std_logic;  -- Start the config of the MAX7219
+      decode_mode_i      : in std_logic_vector(1 downto 0);  -- Decode mode (0x0 - 0x1 - 0x2 - 0x3)
+      intensity_format_i : in std_logic_vector(3 downto 0);  -- Intensity format
+      scan_limit_i       : in std_logic_vector(2 downto 0);  -- Scan limit config
+
+      -- Flag that indicates if the configuration is terminated
+      config_done_o : out std_logic;    -- Config is done
+
+      -- To MAX7219 interface
+      wdata_o       : out std_logic_vector(15 downto 0);  -- Data bus                                        
+      start_frame_o : out std_logic);   -- Start a frame
   end component;
 
 end package pkg_max7219;
