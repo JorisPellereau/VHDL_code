5177f289018cc0efe2ce05031b5ec60b0f33e7f6
JorisP <jor.pell@hotmail.fr>
2020-09-27 21:21:51 +0200

    block for MAX7219 config ongoing

--- MAX7219/sources/lib_max7219_controller/max7219_config_if.vhd
+++ MAX7219/sources/lib_max7219_controller/max7219_config_if.vhd
@@ -6,7 +6,7 @@
 -- Author     : JorisP  <jorisp@jorisp-VirtualBox>
 -- Company    : 
 -- Created    : 2020-09-26
--- Last update: 2020-09-26
+-- Last update: 2020-09-27
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -25,6 +25,9 @@ use ieee.std_logic_arith.all;
 
 entity max7219_config_if is
 
+  generic(
+    G_MAXTRIX_NB : integer := 8
+    );
   port (
     clk   : in std_logic;               -- Clock
     rst_n : in std_logic;               -- Asynchronous clock
@@ -49,21 +52,73 @@ end entity max7219_config_if;
 architecture behv of max7219_config_if is
 
   -- INTERNAL SIGNALS
+
+  signal s_init_config           : std_logic;  -- Init Configuration
+  signal s_new_config_val        : std_logic;
+  signal s_new_config_val_r_edge : std_logic;  -- Rising Edge of New Config. Val
+
   signal s_config_done : std_logic;     -- Config Done
+
+  signal s_cnt_5      : std_logic_vector(2 downto 0);  -- Counter to 5
+  signal s_cnt_5_done : std_logic;                     -- Coutner to 5 reach
+
+  signal s_cnt_matrix      : std_logic_vector(3 downto 0);
+  signal s_cnt_matrix_done : std_logic;
+
+  signal s_max7219_if_start   : std_logic;
+  signal s_max7219_if_en_load : std_logic;
+  signal s_max7219_if_data    : std_logic_vector(15 downto 0);
 
 begin  -- architecture behv
 
 
+  p_latch_inputs : process (clk, rst_n) is
+  begin  -- process p_latch_inputs
+    if rst_n = '0' then                 -- asynchronous reset (active low)
+      s_new_config_val <= '0';
+    elsif clk'event and clk = '1' then  -- rising clock edge
+      s_new_config_val <= i_new_config_val;
+    end if;
+  end process p_latch_inputs;
+
+  -- Rising Edge management
+  s_new_config_val_r_edge <= i_new_config_val and not s_new_config_val;
 
   -- purpose: Configuration Done management
   p_config_done_mngt : process (clk, rst_n) is
   begin  -- process p_config_done_mngt
     if rst_n = '0' then                 -- asynchronous reset (active low)
       s_config_done <= '0';
+
     elsif clk'event and clk = '1' then  -- rising clock edge
 
     end if;
   end process p_config_done_mngt;
+
+
+  p_config_mngt : process (clk, rst_n) is
+  begin  -- process p_config_mngt
+    if rst_n = '0' then                 -- asynchronous reset (active low)
+      s_init_config <= '1';
+      s_cnt_5       <= (others => '0');
+      s_cnt_5_done  <= '0';
+    elsif clk'event and clk = '1' then  -- rising clock edge
+
+      -- Config On Initialization
+      if(s_init_config = '1') then
+        if(i_max7219_if_done = '1') then
+
+          case s_cnt_5 is
+            when "000" =>
+
+            when others => null;
+          end case;
+
+        end if;
+      end if;
+
+    end if;
+  end process p_config_mngt;
 
 
 end architecture behv;
