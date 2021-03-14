9d5d6fe4cce85321db3bfcff5181d8f8fcaef347
JorisP <jor.pell@hotmail.fr>
2021-03-14 11:23:01 +0100

    Up - generation constants ok

--- MAX7219/scripts/MAX7219_memory_generator/mem_gen.py
+++ MAX7219/scripts/MAX7219_memory_generator/mem_gen.py
@@ -335,23 +335,34 @@ class window(QtWidgets.QDialog):
         print("Generer Constant Def")
         data_array = []
         data_array_int = []
+        data_array_final = []
         tmp = 0
+        #offset = 7# Initial Offset
+
+        # INIT Array
+        for i in range(8*self.matrix_nb):
+            data_array_int.append("")
+            data_array_final.append("")
+        
+        #print("data array int : %s \n" % (data_array_int) )
+            
         self.get_checkbox_states()
         for i in range(8*self.matrix_nb):
             data_array.append("")
-            data_array_int.append("")
+            #data_array_int.append("")
             data_array[i] = str(int(self.grid[0][i])) + str(int(self.grid[1][i])) + str(int(self.grid[2][i])) + str(int(self.grid[3][i]))            
             data_array[i] = data_array[i] + str(int(self.grid[4][i])) + str(int(self.grid[5][i])) + str(int(self.grid[6][i])) + str(int(self.grid[7][i]))
 
             
             tmp = (int(self.grid[0][i]) << 7) | (int(self.grid[1][i]) << 6) | (int(self.grid[2][i]) << 5) | (int(self.grid[3][i]) << 4) 
             data_array_int[i] = hex(tmp | (int(self.grid[4][i]) << 3) | (int(self.grid[5][i]) << 2) | (int(self.grid[6][i]) << 1 ) | (int(self.grid[7][i])))
-            
-            #data_array_int[i] = data_array[i]
-            #int(data_array_int[i])
+            #print("tmp : %s \n\n" %(tmp) )
+            tmp = 0
+           
+        #print("data array int : %s \n" % (data_array_int) )
+        #print(type(data_array_int[0]))
 
-        print("data array int : %s \n" % (data_array_int) )
-        print(type(data_array_int[0]))
+        #print("data array : %s \n " %(data_array) )
         
         f = open("constant_gen.vhd", "w")
         f.writelines("type t_cst_array is array (0 to %d) of std_logic_vector(7 downto 0);\n" %(self.matrix_nb * 8 - 1))
@@ -364,6 +375,139 @@ class window(QtWidgets.QDialog):
         f.writelines(");") 
         f.close()
 
+        # Data array final update
+        #offset = 0
+        #for i in range(8*self.matrix_nb):
+            #data_array_final[i*8 + 7 - offset] = str(data_array_int[i])
+            #print("i : %d\n" %(i) )
+            #print("offset : %d\n" %(offset) )
+            #print("(offset*8 - 1) - i : %d\n" %((offset*8 - 1) - i) )
+            #if i == :
+            #    offset = (i*8 - 1)
+
+
+
+        #print(data_array_final)
+
+        digit_7_matrix_n = []
+        digit_6_matrix_n = []
+        digit_5_matrix_n = []
+        digit_4_matrix_n = []
+        digit_3_matrix_n = []
+        digit_2_matrix_n = []
+        digit_1_matrix_n = []
+        digit_0_matrix_n = []
+        
+        # Creates Array
+        for i in range (0, self.matrix_nb):
+             digit_7_matrix_n.append("")
+             digit_6_matrix_n.append("")
+             digit_5_matrix_n.append("")
+             digit_4_matrix_n.append("")
+             digit_3_matrix_n.append("")
+             digit_2_matrix_n.append("")
+             digit_1_matrix_n.append("")
+             digit_0_matrix_n.append("")
+        
+        # == Save in self.grid the state of checkboxes
+        self.get_checkbox_states()
+        tmp_d7 = 0
+        tmp_d6 = 0
+        tmp_d5 = 0
+        tmp_d4 = 0
+        tmp_d3 = 0
+        tmp_d2 = 0
+        tmp_d1 = 0
+        tmp_d0 = 0
+                     
+        # == Fill Matrix af DIGITn Registers
+        for j in range (0, self.matrix_nb):
+
+
+            # Get Collumn 0 (D7 (Mi))
+            tmp_d7 = (int(self.grid[0][j*8]) << 7) | (int(self.grid[1][j*8]) << 6) | (int(self.grid[2][j*8]) << 5) | (int(self.grid[3][j*8]) << 4) 
+            tmp_d7 = hex(tmp_d7 | (int(self.grid[4][j*8]) << 3) | (int(self.grid[5][j*8]) << 2) | (int(self.grid[6][j*8]) << 1 ) | (int(self.grid[7][j*8])))
+            digit_7_matrix_n[self.matrix_nb - 1 - j] = tmp_d7
+
+            #print("tmp_d7 : %s \n" %(tmp_d7) )
+
+            
+            # Get Collumn 1 (D6 (Mi))
+            tmp_d6 = (int(self.grid[0][j*8 + 1]) << 7) | (int(self.grid[1][j*8 + 1]) << 6) | (int(self.grid[2][j*8 + 1]) << 5) | (int(self.grid[3][j*8 + 1]) << 4) 
+            tmp_d6 = hex(tmp_d6 | (int(self.grid[4][j*8 + 1]) << 3) | (int(self.grid[5][j*8 + 1]) << 2) | (int(self.grid[6][j*8 + 1]) << 1 ) | (int(self.grid[7][j*8 + 1])))
+            digit_6_matrix_n[self.matrix_nb - 1 - j] = tmp_d6
+
+            #print("tmp_d6 : %s \n" %(tmp_d6) )
+
+            # Get Collumn 2 (D5 (Mi))
+            tmp_d5 = (int(self.grid[0][j*8 + 2]) << 7) | (int(self.grid[1][j*8 + 2]) << 6) | (int(self.grid[2][j*8 + 2]) << 5) | (int(self.grid[3][j*8 + 2]) << 4) 
+            tmp_d5 = hex(tmp_d5 | (int(self.grid[4][j*8 + 2]) << 3) | (int(self.grid[5][j*8 + 2]) << 2) | (int(self.grid[6][j*8 + 2]) << 1 ) | (int(self.grid[7][j*8 + 2])))
+            digit_5_matrix_n[self.matrix_nb - 1 - j] = tmp_d5
+
+            # Get Collumn 3 (D4 (Mi))
+            tmp_d4 = (int(self.grid[0][j*8 + 3]) << 7) | (int(self.grid[1][j*8 + 3]) << 6) | (int(self.grid[2][j*8 + 3]) << 5) | (int(self.grid[3][j*8 + 3]) << 4) 
+            tmp_d4 = hex(tmp_d4 | (int(self.grid[4][j*8 + 3]) << 3) | (int(self.grid[5][j*8 + 3]) << 2) | (int(self.grid[6][j*8 + 3]) << 1 ) | (int(self.grid[7][j*8 + 3])))
+            digit_4_matrix_n[self.matrix_nb - 1 - j] = tmp_d4
+
+            # Get Collumn 4 (D3 (Mi))
+            tmp_d3 = (int(self.grid[0][j*8 + 4]) << 7) | (int(self.grid[1][j*8 + 4]) << 6) | (int(self.grid[2][j*8 + 4]) << 5) | (int(self.grid[3][j*8 + 4]) << 4) 
+            tmp_d3 = hex(tmp_d3 | (int(self.grid[4][j*8 + 4]) << 3) | (int(self.grid[5][j*8 + 4]) << 2) | (int(self.grid[6][j*8 + 4]) << 1 ) | (int(self.grid[7][j*8 + 4])))
+            digit_3_matrix_n[self.matrix_nb - 1 - j] = tmp_d3
+
+            # Get Collumn 5 (D2 (Mi))
+            tmp_d2 = (int(self.grid[0][j*8 + 5]) << 7) | (int(self.grid[1][j*8 + 5]) << 6) | (int(self.grid[2][j*8 + 5]) << 5) | (int(self.grid[3][j*8 + 5]) << 4) 
+            tmp_d2 = hex(tmp_d2 | (int(self.grid[4][j*8 + 5]) << 3) | (int(self.grid[5][j*8 + 5]) << 2) | (int(self.grid[6][j*8 + 5]) << 1 ) | (int(self.grid[7][j*8 + 5])))
+            digit_2_matrix_n[self.matrix_nb - 1 - j] = tmp_d2
+
+            # Get Collumn 6 (D1 (Mi))
+            tmp_d1 = (int(self.grid[0][j*8 + 6]) << 7) | (int(self.grid[1][j*8 + 6]) << 6) | (int(self.grid[2][j*8 + 6]) << 5) | (int(self.grid[3][j*8 + 6]) << 4) 
+            tmp_d1 = hex(tmp_d1 | (int(self.grid[4][j*8 + 6]) << 3) | (int(self.grid[5][j*8 + 6]) << 2) | (int(self.grid[6][j*8 + 6]) << 1 ) | (int(self.grid[7][j*8 + 6])))
+            digit_1_matrix_n[self.matrix_nb - 1 - j] = tmp_d1
+
+            # Get Collumn 7 (D0 (Mi))
+            tmp_d0 = (int(self.grid[0][j*8 + 7]) << 7) | (int(self.grid[1][j*8 + 7]) << 6) | (int(self.grid[2][j*8 + 7]) << 5) | (int(self.grid[3][j*8 + 7]) << 4) 
+            tmp_d0 = hex(tmp_d0 | (int(self.grid[4][j*8 + 7]) << 3) | (int(self.grid[5][j*8 + 7]) << 2) | (int(self.grid[6][j*8 + 7]) << 1 ) | (int(self.grid[7][j*8 + 7])))
+            digit_0_matrix_n[self.matrix_nb - 1 - j] = tmp_d0
+
+            
+            tmp_d7 = 0
+            tmp_d6 = 0
+            tmp_d5 = 0
+            tmp_d4 = 0
+            tmp_d3 = 0
+            tmp_d2 = 0
+            tmp_d1 = 0
+            tmp_d0 = 0
+
+        #print("\n digit_7_matrix_n : %s \n" % (digit_7_matrix_n) )
+        #print("\n digit_6_matrix_n : %s \n" % (digit_6_matrix_n) )
+        #print("\n digit_5_matrix_n : %s \n" % (digit_5_matrix_n) )
+        #print("\n digit_4_matrix_n : %s \n" % (digit_4_matrix_n) )
+        #print("\n digit_3_matrix_n : %s \n" % (digit_3_matrix_n) )
+        #print("\n digit_2_matrix_n : %s \n" % (digit_2_matrix_n) )
+        #print("\n digit_1_matrix_n : %s \n" % (digit_1_matrix_n) )
+        #print("\n digit_0_matrix_n : %s \n" % (digit_0_matrix_n) )
+            
+        final_array = []
+
+        for i in range(0, 8*self.matrix_nb):
+            final_array.append("")
+
+        for i in range(0, self.matrix_nb):
+            final_array[i] =  digit_0_matrix_n[i]
+            final_array[i + 1*self.matrix_nb] =  digit_1_matrix_n[i]
+            final_array[i + 2*self.matrix_nb] =  digit_2_matrix_n[i]
+            final_array[i + 3*self.matrix_nb] =  digit_3_matrix_n[i]
+            final_array[i + 4*self.matrix_nb] =  digit_4_matrix_n[i]
+            final_array[i + 5*self.matrix_nb] =  digit_5_matrix_n[i]
+            final_array[i + 6*self.matrix_nb] =  digit_6_matrix_n[i]
+            final_array[i + 7*self.matrix_nb] =  digit_7_matrix_n[i]
+
+
+        print("\n\nFinal array : %s\n" %(final_array))
+        #print(len(final_array))
+            
+        
     # Save the status of checkboxes in order to load a specific pattern
     def save_state_checkboxes(self):
         print("Save States checkboxes")
